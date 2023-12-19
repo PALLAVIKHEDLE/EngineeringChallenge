0b985c45f3a2c054a2ec7d27f93737a863b4fb95
PallaviKhedle <pallavi8khedle@gmail.com>
2023-12-18 18:42:10 -0800

    Sync datapoints to cloud

---
+++ backend/syncDataPoints.ts
@@ -0,0 +1,30 @@
+import { historyDataModel } from '../backend/netlify/functions/api/historyData';
+
+export const syncToCloud = (requestBody: any, result: any) => {
+    console.log('cloudsync', requestBody, result);
+
+    const machinesData = requestBody.machines;
+    const scoresData = result.machineScores;
+
+    // Mapping machines data
+    const machineIds = Object.keys(machinesData);
+    const machinesHistoryData = machineIds.map((machineId) => {
+        const scoreValue = scoresData[machineId] || '0.00';
+        console.log('scorevalue',scoreValue)
+
+        return new historyDataModel({
+            machineId: machineId,
+            userId: 'role2',
+            timestamp: new Date(),
+            score: scoreValue,
+            dataPoints: {
+                [machineId]: machinesData[machineId]
+            }
+        });
+    });
+
+
+    console.log('Machines History Data:', machinesHistoryData);
+
+    return machinesHistoryData;
+};
